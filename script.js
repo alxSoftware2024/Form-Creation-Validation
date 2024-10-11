b9665330aa7533d014332c6ec87d0e151f767939
alxsoftware2024 <ashenafin2024@gmail.com>
2024-10-10 22:35:30 -0700

    Input validated

--- script.js
+++ script.js
@@ -7,6 +7,32 @@ document.addEventListener('DOMContentLoaded', function() {
         const username=document.getElementById('username').value.trim();
         const email=document.getElementById('email').value.trim();
         const password=document.getElementById('password').value.trim();
+var isValid=true;
+var messages=[''];
+const validatemail='/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/';
+
+if(username.length<3)
+{
+    isValid=false;
+    messages[0]=['You enter incorrect user'];
+}
+if(!email.match(validatemail))
+{
+    isValid=false;
+    messages[1]=['Email Requared'];
+}
+if(password.length<8)
+{
+    isValid=false;
+    messages[2]=['password length  invalid'];
+}
+if(isValid===true){
+    feedbackDiv.innerHTML="Registration successful!";
+    
+      // If all validations pass, submit the form
+      form.submit();
+}
+
 
     });
 });
