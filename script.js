647f6709cc5031e967ad6af790d9cbddfbac42b6
alxsoftware2024 <ashenafin2024@gmail.com>
2024-10-10 22:08:03 -0700

    Validation page

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Your code here
+    const form=document.getElementById('registration-form');
+    const feedbackDiv=document.getElementById('form-feedback');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent form submission
+        const username=document.getElementById('username');
+        const email=document.getElementById('email');
+        const password=document.getElementById('password');
+
+    });
+});
+    
