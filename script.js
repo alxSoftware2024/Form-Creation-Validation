5dd40c293c75afe206012c367f27ae50635bd22b
alxsoftware2024 <ashenafin2024@gmail.com>
2024-10-12 06:28:55 -0700

    Form validation in js

--- script.js
+++ script.js
@@ -30,25 +30,25 @@ if(password.length<8)
    
 }
 if(isValid){
-    const displayfib=document.getElementById('form-feedback');
-    displayfib.style.display='block';
+    const feedbackDiv=document.getElementById('form-feedback');
+    feedbackDiv.style.display='block';
     feedbackDiv.textContent="Registration successful!";
-    displayfib.style.color='#28a745';
+    feedbackDiv.style.color='#28a745';
       // If all validations pass, submit the form
       form.submit();
 }
 if(!isValid){
-    const displayfib1=document.getElementById('form-feedback');
-    displayfib1.style.display='block';
+    const feedbackDiv=document.getElementById('form-feedback');
+    feedbackDiv.style.display='block';
     //displayfib1.style.color='#dc3545';
        // Join messages with <br> to form a single string
        const feedbackMessage = messages.join('<br>');
     
        // Assign the message to the innerHTML of feedbackDiv
-       displayfib1.innerHTML = feedbackMessage;
+       feedbackDiv.innerHTML = feedbackMessage;
        
        // Set the color of feedbackDiv
-       displayfib1.style.color = "#dc3545";
+       feedbackDiv.style.color = "#dc3545";
     /*for(let i=0;i<=messages.length;i++)
     {
 //console.log(messages[i]);
